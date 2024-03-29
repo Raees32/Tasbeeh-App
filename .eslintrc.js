c430d5167a7491edd5b3d617bbf89e580624089e
Raees Ahmed <raeesahmedawan333@gmail.com>
2024-03-29 22:19:23 +0500

    Initial commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,17 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: ['@react-native', 'plugin:@typescript-eslint/recommended-type-checked'],
+  rules: {
+    
+    // Disable Prettier rule to insert spaces
+    'prettier/prettier': 0,
+    // Disable trailing spaces rule
+    'no-trailing-spaces': 0,
+    // **Important: Disable the base rule as well**
+    'no-unused-vars': ['error', { 'vars': 'all', 'args': 'after-used', 'ignoreRestSiblings': false }],
+    '@typescript-eslint/no-unused-vars': 'error',
+    "usePrettierrc": false
+  },
+  // Disable config file checking (optional)
+  requireConfigFile: false,
 };
--- App.tsx
+++ App.tsx
@@ -1,118 +1,174 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, { useState, useRef } from 'react';
+import { View, Text, TextInput, SafeAreaView, TouchableOpacity, ScrollView, Modal, Pressable } from 'react-native';
+import { StyleSheet } from 'react-native';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+function HomeScreen() {
+  const [count, setCount] = useState(0);
+  const [history, setHistory] = useState<string[]>([]);
+  const [modalVisible, setModalVisible] = useState(false);
+  const descriptionInputRef = useRef(null);
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  const handleCount = () => {
+    setCount(count + 1);
+  };
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  const handleReset = () => {
+    setCount(0);
+    saveHistory(count);
+  };
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+  const saveHistory = (countValue: number) => {
+    const historyItem = `Count ${history.length + 1}) ${countValue} times`;
+    setHistory([...history, historyItem]);
+  };
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const handleHistoryPress = () => {
+    setModalVisible(true);
+  };
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const handleModalClose = () => {
+    setModalVisible(false);
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.header}>Tasbeeh Counter</Text>
+     
+      <Text style={styles.counter}>{count}</Text>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity onPress={handleCount}>
+          <Text style={[styles.buttonText, { backgroundColor: '#4CAF50' }]}>COUNT</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={handleHistoryPress}>
+          <Text style={[styles.buttonText, { backgroundColor: '#2196F3' }]}>HISTORY</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={handleReset}>
+          <Text style={[styles.buttonText, { backgroundColor: '#FF5733' }]}>RESET</Text>
+        </TouchableOpacity>
+      </View>
+      <TextInput  
+        ref={descriptionInputRef}
+        style={styles.messageInput}
+        placeholder={'Description Add help for using App'}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={handleModalClose}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalHeader}>History</Text>
+            <ScrollView style={styles.historyContainer}>
+              {history.map((item, index) => (
+                <Text key={index} style={styles.historyItem}>{item}</Text>
+              ))}
+            </ScrollView>
+            <Pressable onPress={handleModalClose}>
+              <Text style={styles.closeButton}>Close</Text>
+            </Pressable>
+          </View>
         </View>
-      </ScrollView>
+      </Modal>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'black',
+
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  header: {
+    fontSize: 35,
+    color: '#ffffff',
+    backgroundColor: 'red',
+  paddingHorizontal: 73,
+    top:28,
+    fontWeight: '400'
+
   },
-  sectionDescription: {
-    marginTop: 8,
+ 
+  counter: {
+    fontSize: 40,
+    fontWeight: 'bold',
+   
+    backgroundColor: 'yellow',
+    padding: 10,
+    paddingHorizontal:30,
+    borderRadius: 5,
+    marginTop:165
+
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 170,
+   
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 21,
+    width: 100,
+    height: 35,
+    borderRadius: 7,
+    textAlign: 'center',
+    paddingVertical: 5,
+    margin: 10,
+    fontWeight:'500',
+    top: 63,
+   
+  },
+  messageInput: {
+    top: 30,
+    height: '20%',
+    width: '100%',
+    borderWidth: 1,
+    marginBottom:100,
+    paddingHorizontal: 10,
+    backgroundColor:'yellow',
+  fontWeight:'400',
+    fontSize: 20,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+
+  },
+  modalContent: {
+    backgroundColor: '#fff',
+    padding: 20,
+    borderRadius: 10,
+    maxHeight: '80%',
+    width: '80%',
+    
+  },
+  modalHeader: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    
+  },
+  historyContainer: {
+    maxHeight: 200,
+    marginBottom: 10,
+  },
+  historyItem: {
     fontSize: 18,
-    fontWeight: '400',
+    marginTop: 5,
   },
-  highlight: {
-    fontWeight: '700',
+  closeButton: {
+    fontSize: 18,
+    color: 'blue',
+    textAlign: 'center',
   },
 });
 
-export default App;
+export default HomeScreen;
